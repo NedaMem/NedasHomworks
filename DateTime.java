5656e23890f504c6043ac3d8aa3dc02b50a8833b
NedaMem <neda.memar@gmail.com>
2015-01-30 13:08:45 +0100

    1st program

---
+++ DateTime.java
@@ -0,0 +1,39 @@
+package DateTime ;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.swing.JOptionPane;
+public class DateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+     //Date date = new Date();
+   //  System.out.println(date.toString());
+		
+		//JOptionPane.showMessageDialog(null, new Date());
+		
+	
+		
+		LocalDateTime current = LocalDateTime.now();
+		System.out.println("The current date is " + current.getDayOfMonth() +
+				"/" + current.getMonthValue() + "/" 
+				+ current.getYear());
+		
+		System.out.println("The current time is " + current.getHour()+
+				":" + current.getMinute() + ":" 
+				+ current.getSecond());
+		
+		
+ 
+     
+     
+     
+    		 
+     
+	}
+
+}
