0a76e223c85a3b1ddb0ac2330bb75e72ad1e895b
NedaMem <neda.memar@gmail.com>
2015-04-13 15:05:47 +0200

    2St Program 13 mars

---
+++ OutOfMemoryError.java
@@ -0,0 +1,15 @@
+package therteenmars;
+
+public class OutOfMemoryError {
+	public static void main(String[] args) {
+
+		Long maxMemory = Runtime.getRuntime().maxMemory();
+		System.out.println(maxMemory);
+
+		int[] matrix = new int[(int) (maxMemory + 1)];
+			for (int i = 0; i < matrix.length; ++i)
+			matrix[i] = i + 1;
+
+	}
+
+}
